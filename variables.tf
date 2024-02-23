6561ee329388e27f5baf7f9c7f4649d6a241986f
Anbazhagansekar-cloud <160880709+Anbazhagansekar-cloud@users.noreply.github.com>
2024-02-23 18:03:18 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  type =string
+  default = "ap-south-1"
+  
+}
+
+variable "subnet_id_1" {
+
+  type = string
+
+  default = "subnet-0f439b8c3da156844"
+
+ }
+ variable "subnet_id_2" {
+
+  type = string
+
+  default = "subnet-0a4348cebe7bc64fd"
+
+ }
+ variable "subnet_id_3" {
+  
+  type = string
+
+  default = "subnet-06e26fe835c43628a"
+
+ }
+
+ variable "vpc_id" {
+  type = string
+  default = "vpc-0ae8f8ec41966e387"
+   
+ }
