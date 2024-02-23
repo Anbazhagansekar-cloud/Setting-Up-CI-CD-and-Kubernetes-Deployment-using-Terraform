205ddd4c82018e1a5387ae3953096e4f50c52ee9
Anbazhagansekar-cloud <160880709+Anbazhagansekar-cloud@users.noreply.github.com>
2024-02-23 18:55:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,26 +8,26 @@ variable "subnet_id_1" {
 
   type = string
 
-  default = "subnet-0f439b8c3da156844"
+  default = "subnet-023f849d3e55404b9"
 
  }
  variable "subnet_id_2" {
 
   type = string
 
-  default = "subnet-0a4348cebe7bc64fd"
+  default = "subnet-089aee5fe7d8657f1"
 
  }
  variable "subnet_id_3" {
   
   type = string
 
-  default = "subnet-06e26fe835c43628a"
+  default = "subnet-0f9972eaca42ef357"
 
  }
 
  variable "vpc_id" {
   type = string
-  default = "vpc-0ae8f8ec41966e387"
+  default = "vpc-06686f02fe260db90"
    
  }
